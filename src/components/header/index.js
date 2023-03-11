dbd926b8ad3eeccbdf104d44ee23ec7cc5fe08e5
davidgamaserrate1 <david.gama@ufms.br>
2023-03-11 18:30:39 -0300

    ref: design header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,16 @@
 import './header-styles.css'
-
-import logoImage from '../../assets/logo.png'
+import logo from '../../assets/logo.png'
 
 const Header = ()=>{
     return (
-    <header className='header'>
-      <div className='header-name'>
-        <div className='header-name_desc' >Cadastro De Clientes</div>
-        
-        <div className='header-logo' > 
-          <img className='header-img' src={logoImage} />  
-        </div>
-      </div>
-    </header>
+     <>
+     <div className='header-out'>
+     </div>
+       <header className='header'>
+       <div className='header-desc'> Cadastro De Clientes </div>
+       <div className='header-logo'>  <img  className='header-logo__image' src={logo}/> </div>
+     </header>
+     </>
     )
 }
 
