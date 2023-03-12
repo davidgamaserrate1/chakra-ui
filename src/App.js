f609a3f59ad0285e9fb76a978fadd71072cc17da
davidgamaserrate1 <david.gama@ufms.br>
2023-03-12 08:17:10 -0300

    feat: listing clients using api data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Header from './components/header/';
 import ListClients from './components/listClients/';
 import Banner from './components/banner';
-
+ 
 function App() {
   return (
      <div>
         <Header/>
-         <Banner desc ='Cadastro De Clientes'/>
-          
+         <Banner desc ='Cadastro De Clientes'/>          
         <ListClients/>
      </div>
   );
