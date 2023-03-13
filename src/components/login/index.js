61460eedd46dfed4ebbf86467681345d2a7d49d1
david.serrate <david.serrate@redetendencia.com.br>
2023-03-13 17:44:06 -0400

    feat: login page

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,20 +1,21 @@
 import './login-styles.css'
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
     return (
         <div className="login-container"   >
-
+            <div className='login-logo'> <img className='login-logo__image' src={logo} alt='logo' /> </div>
 
             <div className="login-card">
                 <form className="login-form" onSubmit={null}>
                     <label className="login-form__name"> Usuário </label>
                     <input className="login-form__name-input" type="text" name="user" />
-
                     <label className="login-form__password"> Senha </label>
                     <input className="login-form__name-input" type="password" name="password" />
+                    <div className="login-button" >Entrar</div>
                 </form>
+
             </div>
 
 
